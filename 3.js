f54f085d91cbc4ddd9e6bdd397b6273211b39e47
kulalkoushik <koushikkulal715@gmail.com>
2024-09-19 12:09:56 +0530

    auhb

---
+++ 3.js
@@ -0,0 +1,8 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    if (name
